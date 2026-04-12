f62351aee5553e26c4d30adbf707ddbde3011bd6
Pelemenguin <elepentuin@outlook.com>
2026-04-12 15:03:36 +0800

    End project `ProxyJS` due to a Rhino problem

--- kubejs/startup_scripts/Proxy/ProxyJS.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace ProxyJS {
-
-    const Proxy: typeof globalThis.Proxy;
-    function proxy<T>(target: T, handler: ProxyHandler<T>): T;
-
-}
-
-declare const ProxyJS: typeof ProxyJS;
